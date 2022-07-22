c7ac4f4454e6d18b045e030930efc073920fdec5
OdidiLavender <lavender.odidi@student.moringaschool.com>
2022-07-22 19:03:27 +0300

    added the cook time to the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+// import { RatingModule } from 'ng-starrating';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,HomeComponent
+    HeaderComponent,
+    HomeComponent
 
   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // RatingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
